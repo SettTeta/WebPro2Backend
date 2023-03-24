f22115bc12ce3348b07fc9cc8e63ec68e0b16ab0
Sett Teta <stetanonsakul@gmail.com>
2023-03-24 21:08:49 +0700

    revert back

--- pages/api/hub/students/index.js
+++ pages/api/hub/students/index.js
@@ -5,11 +5,6 @@ export default async function handler(req, res) {
     await connect(connectionString);
     console.log("req.method: ", req.method)
 
-    // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
     if (req.method === 'GET') {
         const docs = await Student.find();
         res.status(200).json(docs)
